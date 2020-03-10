a2127ef06354d47dd3e81dfe8e03e4de0876f41e
iamcristos <nmereginivincent@gmail.com>
2020-03-10 08:26:46 +0100

    created product schema

--- context/authenticate.user.js
+++ context/authenticate.user.js
@@ -3,7 +3,6 @@ const { decodeToken } = require('../utils/token.jwt');
 async function authenticateUser(token, model) {
     try {
         const verifyToken = await decodeToken(token);
-        console.log(verifyToken)
         const user = await model.user.findUser(verifyToken.data);
         return !user ? null : user
     } catch (error) {
--- typedefs/product.schema.js
+++ typedefs/product.schema.js
@@ -1,12 +1,81 @@
 const { gql } = require("apollo-server");
 
 const Product = gql`
-    type Product {
+    enum AllowedProduct {
+        animal
+        plant
+    }
+
+    enum AllowedAnimal {
+        livestock
+        birds
+    }
+
+    enum AllowedPlant {
+        cereal
+        fruits
+        tubers
+        others
+    }
+    interface Product {
+        id: Float!
         name: String!
+        description: String!
+        user: User!
+        price: Float!
+        type: AllowedProduct!
+        age: Float!
+    }
+    type Animal implements Product{
+        id: Float!
+        name: String!
+        description: String!
+        user: User!
+        price: Float!
+        type: AllowedProduct!
+        age: Float!
+        weight: Float!
+        height: Float
+        animalType: AllowedAnimal!
+    }
+
+    type Plant implements Product{
+        id: Float!
+        name: String!
+        description: String!
+        user: User!
+        price: Float!
+        type: AllowedProduct!
+        length: Float!
+        plantType: AllowedPlant!
+    }
+
+    input CreateInput {
+        id: Float!
+        name: String!
+        description: String!
+        user: User!
+        price: Float!
+        type: AllowedProduct!
+        age: Float
+        weight: Float
+        height: Float
+        animalType: AllowedAnimal
+        length: Float
+        plantType: AllowedPlant
+    }
+
+    input IdInput {
+        id: Float!
     }
 
     type Query {
-        product: Product
+        product(input: IdInput!): Product
+        products: [Product]
+    }
+
+    etend type Mutation{
+        createProduct(input: CreateInput!): Product!
     }
 
 `;
