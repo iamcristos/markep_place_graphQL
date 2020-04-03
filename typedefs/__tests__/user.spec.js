2c12b48205f61292802977b5f27a0412df770eda
iamcristos <nmereginivincent@gmail.com>
2020-04-03 09:29:13 +0100

    added tests

--- typedefs/__tests__/user.spec.js
+++ typedefs/__tests__/user.spec.js
@@ -18,5 +18,34 @@ describe('User Schema', () =>{
        expect(response.errors).not.toBeTruthy()
        done()
     })
+
+    test('should error out if email is not provided', async done =>{
+        const server = mockServer(typeDefs)
+        const query = `{
+            user() {
+                email
+                id
+            }
+        }`;
+
+        const resolve = await server.query(query)
+        expect(resolve.errors).toBeTruthy()
+        done()
+    })
+
+    test('should resolve login', async done => {
+        const server = mockServer(typeDefs)
+        const query = `{
+            loginUser(input:{email: "g@gmail.com" password: "12345"}) {
+                email
+                id
+            }
+        }`;
+
+        const response = await server.query(query)
+        await expect(server.query(query)).resolves.toBeTruthy()
+        expect(response.errors).not.toBeTruthy()
+        done()
+    })
     
 })
